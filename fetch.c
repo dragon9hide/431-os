c5d9c82086e64f6d1f1721cb50b6bcbdfe992a61
dragon9hide <erogon_ben10@yahoo.com>
2021-02-19 19:51:39 +0000

    yes

--- fetch.c
+++ fetch.c
@@ -2,20 +2,26 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <stdlib.h>
+#include <pthread.h>
 
-int main(int argc, char *argv[]) {
-	for(int i = 1; i < argc; i++){	
+int main(int argc, char* argv[]) {
+	if (argc < 1) {
+        execlp("/usr/bin/wget", "wget", "index.html", NULL);;
+        return 0;
+  }
+  for(int i = 1; i < argc; i++){	
 		int pid = fork();
 		if (pid > 0) {
 			// Parent
-			printf("waiting for web download");
+			printf("waiting for web download \n");
 			int child_pid = wait(NULL);
-			printf("webpage downloaded");
+			printf("webpage downloaded \n");
 		}
 		else if (pid == 0) {
-			char url[] = argv[i];
-			execlp("/usr/bin/wget", "wget", url, NULL);
+			execlp("/usr/bin/wget", "wget", argv[i], NULL);
 			exit(0);
 		}
+  }
+  printf("\n");
 	return 0;
 }
